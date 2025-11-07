754b8d9a9a8243520b78b375b1c033b4508a4149
hhannsterr <rprasoprat@gmail.com>
2025-11-07 11:52:54 +0700

    add config

--- fetch_data.sh
+++ fetch_data.sh
@@ -2,13 +2,14 @@
 
 echo "Starting Foxpro Data Fetch..."
 
+DATA_PATH="D:\BIS"
 VFP_PATH="C:\\Program Files (x86)\\Microsoft Visual FoxPro 9\\vfp9.exe"
 USERNAME="your_username"
 PASSWORD="your_password"
 
 TEMP_PRG=$(mktemp).prg
 cat > "$TEMP_PRG" << EOF
-SET DEFAULT TO "D:\BIS"
+SET DEFAULT TO $DATA_PATH
 
 KEYBOARD '{ENTER}{TAB}$USERNAME{ENTER}$PASSWORD{ENTER}'
 
