e21af73a78a1ccacf4e90d4c0c284301bc220d3a
hhannsterr <rprasoprat@gmail.com>
2025-11-07 11:14:24 +0700

    add bash script to automate FoxPro data extraction

---
+++ fetch_data.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+echo "Starting Foxpro Data Fetch..."
+
+VFP_PATH="C:\\Program Files (x86)\\Microsoft Visual FoxPro 9\\vfp9.exe"
+USERNAME="HAN"
+PASSWORD="12345"
+
+TEMP_PRG=$(mktemp).prg
+cat > "$TEMP_PRG" << EOF
+SET DEFAULT TO "D:\BIS"
+
+KEYBOARD '{ENTER}{TAB}$USERNAME{ENTER}$PASSWORD{ENTER}'
+
+DO pp.prg
+DO dosummary.prg
+QUIT
+EOF
+
+"$VFP_PATH" "$TEMP_PRG"
+rm -f "$TEMP_PRG"
